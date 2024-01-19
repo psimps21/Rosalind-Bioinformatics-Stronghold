3db4fff8f16c44770d2d797748d01ba3699785a0
Parker Simpson <psimpson@andrew.cmu.edu>
2024-01-19 14:35:24 -0500

    refactored code

--- problems/PERM.py
+++ problems/PERM.py
@@ -3,11 +3,15 @@ Given: A positive integer n≤7.
 
 Return: The total number of permutations of length n, followed by a list of all such permutations (in any order).
 """
+import sys
+
+def ReadInput(input_f):
+    with open(input_f, 'r') as f:
+        data = [line.strip() for line in f.readlines()]
+
+    return int(data[0])
 
 def perms(lst):
-    if len(lst) == 0:
-        return []
-
     if len(lst) == 1:
         return [lst]
 
@@ -26,12 +30,15 @@ def find_perms(n):
     lst = [*range(1,n+1)]
     perm_list = perms(lst)
 
-    return perm_list, len(perm_list)
+    return perm_list
 
 
 if __name__ == '__main__':
-    # n = input('Enter a value for n: ')
-    n=3
-    perms, nums = find_perms(n)
-    print(nums)
-    [print(*p) for p in perms]
+    # input_f = sys.argv[1]
+    # num = ReadInput(input_f)
+    num = 3
+
+    perms_list = find_perms(num)
+
+    print(len(perms_list))
+    [print(*p) for p in perms_list]
