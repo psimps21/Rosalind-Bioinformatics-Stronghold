ff9760898df98c95eec5307d71c9f1b921592bfd
Parker Simpson <psimpson@andrew.cmu.edu>
2024-01-18 22:52:48 -0500

    refactored code

--- problems/MRNA.py
+++ problems/MRNA.py
@@ -4,7 +4,13 @@ Given: A protein string of length at most 1000 aa.
 Return: The total number of different RNA strings from which the protein could have been translated, modulo 1,000,000.
 (Don't neglect the importance of the stop codon in protein translation.)
 """
+import sys
 
+def ReadInput(input_f):
+    with open(input_f, 'r') as f:
+        data = f.readlines()
+
+    return data[0].strip()
 
 def posibble_mrnas(aa_seq):
     aa_codon_dict = {
@@ -30,14 +36,16 @@ def posibble_mrnas(aa_seq):
          'W': ['UGG'],
          'Y': ['UAU', 'UAC']}
 
-    mod = 1
+    mod = 3 # number of stop codons
     for aa in aa_seq:
         mod *= len(aa_codon_dict[aa])
 
-    return mod * 3 % 1000000
+    return mod % 1000000
 
 
 if __name__ == '__main__':
-    prot = input('Enter a protein sequence: ')
-    print(posibble_mrnas(prot))
+    input_f = sys.argv[1]
+    aa_seq = ReadInput(input_f)
+
+    print(posibble_mrnas(aa_seq))
 
