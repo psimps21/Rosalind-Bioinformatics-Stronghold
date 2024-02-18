d67006ea155c494ad92445bf7f99fadf9393e91e
Parker Simpson <psimpson@andrew.cmu.edu>
2024-02-18 15:05:25 -0500

    refectored REVP

--- problems/REVP.py
+++ problems/REVP.py
@@ -1,3 +1,8 @@
+"""
+Given: A DNA string of length at most 1 kbp in FASTA format.
+
+Return: The position and length of every reverse palindrome in the string having length between 4 and 12. You may return these pairs in any order.
+"""
 import sys
 
 def ReadInput(input_f):
